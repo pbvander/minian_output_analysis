1b83b57710c72657d1bbe5657b5b50642ae30baf
Paul Vander <pbvander24@gmail.com>
2025-10-22 08:41:19 -0700

    Create initial analysis pipeline
    Create initial pipeline with supporting functions

---
+++ functions.R
@@ -0,0 +1,220 @@
+##Miniscope data
+read_csv_minian <- function(file){
+  d<-read_csv(file, show_col_types = F)
+  d<-d%>%mutate(experiment=exp,
+                mouse=mouse,
+                start_date=start_date,
+                session=session)%>%
+    filter(unit_id %nin% bad_cells)
+  return(d)
+}
+
+read_cell_label <- function(file){
+  cell_label<-read_csv(file,show_col_types = F)%>%rename(label = "1")
+  cell_label$unit_id<-0:(nrow(cell_label)-1)
+  bad_cells<-cell_label%>%filter(label==0)%>%pull(unit_id)
+}
+
+read_timestamps <- function(file){
+  d<-read_csv(file, show_col_types = F)
+  if("frame" %nin% colnames(d)){d<-d%>%rename(frame = `Frame Number`, time_ms = `Time Stamp (ms)`, buffer_index = `Buffer Index`, start_time = start_time)}
+  return(d)
+}
+
+scale_temporal <- function(d){
+  d<-d%>%group_by(unit_id)%>%mutate(scaled_C = C/max(C),
+                                    scaled_S = S/max(S),
+                                    scaled_YrA = YrA/max(YrA))
+  return(d)
+}
+
+read_motion <- function(path){
+  pattern<-"motion[0-9]*.csv"
+  if (!any(grepl(pattern, list.files(path)))){stop("No motion files detected")}
+  files<-grep(pattern, list.files(path),value=T)%>%mixedsort()
+  if (length(files) == 1){d<-read_csv_minian(paste0(path,separator,files))}
+  if (length(files) > 1){
+    d<-tibble()
+    f<-0
+    for (file in files){
+      m<-read_csv(paste0(path,separator,file), show_col_types = F)%>%filter(!is.na(height) & !is.na(width))
+      m$frame <- (0:(nrow(m)-1)) + f
+      d<-rbind(d,m)
+      f<-f + nrow(m)
+    }
+  }
+  d<-d%>%mutate(motion_distance = sqrt(height^2 + width^2))%>%rename(motion_width = width, motion_height=height)
+  return(d)
+}
+
+##Telemetry
+read_telemetry_data <- function(file, format = "starr-lifesci", idinfo = c("mouse", "misc", "measure", "misc2"), round = F, round_unit = "00:01:00"){
+  d<-read_csv(file, show_col_types = F)
+  if (format == "starr-lifesci"){ ###For STARR Life Sciences VitalView software output
+    idinfo<-c("mouse", "misc", "measure", "misc2")
+    colnames(d)<-gsub(" ", "_", colnames(d))#remove spaces from column names and add underscores
+    d<-d%>%
+      pivot_longer(3:length(colnames(d)),names_to="ID",values_to="data")%>%
+      separate(ID, sep="_",idinfo)%>% ##Ignore warning about additional pieces
+      select(!starts_with("misc"))%>%
+      mutate(measure=case_when(measure=="Deg."~"temp",measure=="Cnts"~"act",T~"unknown"))%>%
+      mutate(chron=chron(dates.=date, times.=time))
+    if (round){d<-d%>%mutate(chron = chron%>%round(units=times(round_unit)))}
+    if (any(grepl(d$measure, pattern="unknown"))){stop("Unknown measure in telemetry data")}
+    d<-d%>%spread(measure,data)
+    
+  } else {
+    stop("This function currently only supports 'starr-lifesci' as a format")
+  }
+  return(d)
+}
+
+##Saving plots/data
+save_plot<- function(name, plot=last_plot(), direc="./output/",w=NA,h=NA,units="in", ...){
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,name,".pdf"), plot = plot, width=w, height=h, units=units, ...)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,name,".svg"), plot = plot, width=w, height=h, units=units, ...)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,name,".png"), plot = plot, width=w, height=h, units=units, ...)
+  # print("Saving RDS...")
+  # saveRDS(object=last_plot(), file = paste0(direc,name,".rds"))
+}
+
+save_plot_lowdpi <- function(name, plot=last_plot(), direc="./output/",w=NA,h=NA,units="in"){
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,name,".pdf"), plot = plot, width=w, height=h, units=units,dpi=72)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,name,".svg"), plot = plot, width=w, height=h, units=units)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,name,".png"), plot = plot, width=w, height=h, units=units)
+  # print("Saving RDS...")
+  # saveRDS(object=last_plot(), file = paste0(direc,name,".rds"))
+}
+
+save_plot_facet <- function(name,facet_name=NULL, plot=last_plot(), by="mouse", direc="./output/",w=NA,h=NA,fw=NA,fh=NA,units="in"){
+  #Define facet name
+  if (is.null(facet_name)){facet_name<-paste(name, "by", by)}
+  
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,name,".pdf"), plot = plot, width=w, height=h, units=units)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,name,".svg"), plot = plot, width=w, height=h, units=units)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,name,".png"), plot = plot, width=w, height=h, units=units)
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,facet_name,".pdf"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,facet_name,".svg"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,facet_name,".png"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving RDS...")
+  # saveRDS(object=last_plot(), file = paste0(direc,name,".rds"))
+}
+
+save_plot_facet_exp <- function(name,facet_name=NULL, plot=last_plot(), by="exp", direc="./output/",w=NA,h=NA,fw=NA,fh=NA,units="in"){
+  #Define facet name
+  if (is.null(facet_name)){facet_name<-paste(name, "by", by)}
+  
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,name,".pdf"), plot = plot, width=w, height=h, units=units)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,name,".svg"), plot = plot, width=w, height=h, units=units)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,name,".png"), plot = plot, width=w, height=h, units=units)
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,facet_name,".pdf"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving svg...")
+  ggsave(filename = paste0(direc,facet_name,".svg"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving png...")
+  ggsave(filename = paste0(direc,facet_name,".png"), plot = facet(plot,by), width=fw, height=fh, units=units)
+  # print("Saving RDS...")
+  # saveRDS(object=last_plot(), file = paste0(direc,name,".rds"))
+}
+
+save_pdf <- function(name, plot=last_plot(), direc="./output/",w=NA,h=NA,units="in"){
+  # print("Saving pdf...")
+  ggsave(filename = paste0(direc,name,".pdf"),plot = plot, width=w, height=h, units=units)
+}
+
+save_png_large <- function(name, plot=last_plot(), direc="./output/",w=NA,h=NA,units="in",dpi=300){ #to be used with very large plots (raster plots is what I wrote this for). Using ggsave seems to cause issues with color
+  png(filename = paste0(direc,name,".png"), width=w, height=h, res=dpi, units=units)
+  print(plot)
+  dev.off()
+}
+
+stat_save<- function(data, name=NA, direc="./output/"){
+  if ("groups" %in% colnames(data)){
+    data$groups<-as.character(data$groups)}
+  write_csv(x=data, file=paste0(direc,ifelse(is.na(name),deparse(substitute(data)),name),".csv"))}
+
+write_output<-function(data, direc="./output/", name = NA){
+  saveRDS(object=data, file=paste0(direc,ifelse(is.na(name),deparse(substitute(data)),name),".rds"))
+  write_csv(x=data, file=paste0(direc,ifelse(is.na(name),deparse(substitute(data)),name),".csv"))}
+
+##ggplot themes/aesthetics/settings
+point_summary <- function(...,size=8){
+  geom_point(size=size,stat="summary",...)
+}
+
+point_indiv <- function(...,fill="grey60",size=3,seed=123,position=position_jitter(width=0.05,height=0,seed=seed),shape=21,color="grey20",alpha=0.7){
+  geom_point(fill=fill,size=size,position=position,shape=shape,color=color,alpha=alpha,...)
+}
+
+point_errorbar <- function(...,width=0.35,size=1,color="grey15"){
+  geom_errorbar(stat="summary",width=width,size=size,color=color,...)
+}
+
+continuous_line <- function(...,stat="summary",size=1.5){
+  geom_line(size=size,stat=stat,...)
+}
+
+continuous_errorbar<- function(...,width=0,alpha=0.2){
+  geom_errorbar(stat="summary",width=width,alpha=alpha,...)
+}
+
+xy_point <- function(...,size=4){
+  geom_point(size=size,...)
+}
+
+xy_point2 <- function(...,size=3,shape=21,color="grey20",alpha=0.7){
+  geom_point(size=size,shape=shape,color=color,alpha=alpha,...)
+}
+
+regression_line <- function(..., size=1.5, method="lm", se=F){
+  geom_smooth(size=size,method = method, se = se,...)
+}
+  
+line_error <- function(..., width=0,alpha=0.3){
+  geom_errorbar(width=width,alpha=alpha,stat="summary",...)
+}
+
+line_pair <- function(..., color="grey",seed=123,position=position_jitter(width=0.05,height=0,seed=seed),size=1,alpha=0.8){
+  geom_path(color=color,position=position,size=size,alpha=alpha,...)
+}
+
+draw_pvalue <- function(..., data,label.size=6,bracket.size=1){
+  stat_pvalue_manual(data=data,label.size=label.size,bracket.size=bracket.size,...)
+}
+
+annotate_pvalue <- function(..., geom="text",hjust=0,x=0,y=0,p,size=4.7,fontface="bold"){ #set p equal to cell in t_test where p-value is located!
+  annotate(geom=geom,hjust=hjust,x=x,y=y,label=paste0("p=",p),size=size,fontface=fontface,...)
+} 
+
+annotate_text <- function(..., geom="text",hjust=0,x=0,y=0,label="Lorem Ipsum",size=4.7,fontface="bold"){
+  annotate(geom=geom,hjust=hjust,x=x,y=y,label=label,size=size,fontface=fontface,...)
+}
+
+##Misc
+`%nin%` <- Negate(`%in%`)
+
+pca_matrix<- function(df, row_names_from = 1){
+  df<-as.data.frame(df)
+  rownames(df)<-df[,row_names_from]
+  df<-df%>%select(!row_names_from)
+  return(df)
+}
+
+write_sessioninfo<- function(){
+  writeLines(capture.output(sessionInfo()), "./output/sessionInfo.txt")
+}
---
+++ miniscope_telemetry analysis.R
@@ -0,0 +1,117 @@
+#####This code is written by PV. It is meant to read in the output from PV_minian package, integrate this data with telemetry measurements and annotated events, and visualize the data all together.
+
+library(tidyverse)
+library(ggpubr)
+library(ggprism)
+library(ggridges)
+library(rjson)
+library(nlme)
+library(chron)
+library(gtools)
+summarize<-dplyr::summarize
+source("C:/Users/paulv/Documents/GitHub/minian_output_analysis/functions.R")
+
+##### Things to set manually:
+#Paths/directories:
+exp_direc<-"C:/Users/paulv/Box/correalab/Member Folders/Paul Vander/Experiments"
+output_dir<-"C:/Users/paulv/Box/correalab/Member Folders/Paul Vander/Data/Torpor project cross-experiment analyses/Miniscope"
+direcs<-c("250417_circulating_E2_torpor_miniscope/pre-OVX_torpor")
+dir_struct<-c("test","mouse","start_date","session","start_time","camera") #set this to the levels of metadata stored in the directory structure of the data. Set in order from the first/higher/root directory level, to the last/lowest/final branch directory level
+separator<-"/" #set depending on OS
+
+#Data to exclude:
+mice_to_exclue<-c()
+session_ids_to_exclude<-c()
+
+#Global graph settings:
+ms<-theme_prism()
+
+##### Read in miniscope data:
+#Initalize variables
+A<-tibble()
+C<-tibble()
+S<-tibble()
+YrA<-tibble()
+motion<-tibble()
+ts<-tibble()
+
+#Read in csv files
+setwd(exp_direc)
+for (dir in direcs){
+  # print(paste0(".",dir))
+  for (mouse in list.dirs(dir, full.names=F,recursive=F)){
+    # print(paste0("..",mouse))
+    for (start_date in list.dirs(paste(dir,mouse,sep=separator),full.names=F,recursive=F)){
+      # print(paste0("...",start_date))
+      for (session in list.dirs(paste(dir,mouse,start_date,sep=separator),full.names=F,recursive=F)){
+        # print(paste0("....",session))
+        for (start_time in list.dirs(paste(dir,mouse,start_date,session,sep=separator),full.names=F,recursive=F)){
+          if ("A.csv" %in% list.files(paste(dir,mouse,start_date,session, start_time,sep=separator))){
+            path = paste(dir,mouse,start_date,session,start_time,sep=separator)
+            print(paste0("Reading ", path))
+            exp<-strsplit(strsplit(dir,separator)[[1]][1],"_")[[1]][1]
+            bad_cells<-read_cell_label(paste0(path,separator,"cell_label.csv"))
+            A<-rbind(A,read_csv_minian(paste0(path,separator,"A.csv"))%>%filter(A>0))
+            C<-rbind(C, read_csv_minian(paste0(path,separator,"C.csv")))
+            S<-rbind(S, read_csv_minian(paste0(path,separator,"S.csv")))
+            YrA<-rbind(YrA, read_csv_minian(paste0(path,separator,"YrA.csv")))
+            motion<-rbind(motion, read_motion(path))
+            ts<-rbind(ts, read_timestamps(paste(path,"My_V4_Miniscope","timeStamps.csv", sep=separator)))
+          }
+        }
+      }
+    }
+  }
+}
+
+#Create dataframe with all timeseries data
+df<-merge(C,S)%>%
+  merge(YrA)%>%
+  merge(ts)%>%
+  mutate(start_chron = chron(dates.=start_date, times.=start_time, format=c(dates="y_m_d",times = "h_m_s"),out.format=c(date="m-d-y",time="h:m:s")),
+                                                 chron = start_chron + (time_ms / (1000*60*60*24)),
+                                                 init_unit_id = unit_id, #preserve original unit_id labels)
+                                                 unit_id = factor(unit_id)%>%as.numeric()%>%factor() #renumbers unit_id starting from 1
+                                                 )%>%
+  scale_temporal()
+
+#Clean up variables and memory
+rm(C,S,YrA,ts)
+gc()
+
+##### Read in telemetry data
+read<-c()
+t_df<-tibble()
+for (dir in direcs){
+  telem_file<-paste(strsplit(direcs[1],separator)[[1]][1],"telem data","telem data combined.csv", sep=separator)
+  if (telem_file %in% read){next}
+  print(telem_file)
+  t_df<-rbind(t_df, read_telemetry_data(telem_file))
+  read <- c(read, telem_file)
+}
+
+##### Merge telemetry and miniscope data
+#Create time bin column as basis for merging
+df<-df%>%mutate(chron_bin = cut(chron, seq(min(t_df$chron)-1/(24*60*2),max(t_df$chron)+(1/(24*60*2)), (1/(24*60)))))
+t_df<-t_df%>%mutate(chron_bin = cut(chron, seq(min(t_df$chron)-(1/(24*60*2)),max(t_df$chron)+(1/(24*60*2)), (1/(24*60)))))
+
+#Preserve all miniscope data, and assign one temperature/act to all frames in the given range
+df<-merge(df, t_df, by=c("mouse","chron_bin"), all=T)
+df<-rbind(df%>%filter(is.na(chron.x))%>%rename(chron=chron.y)%>%select(!chron.x), #chron.x is time measured from miniscope data, chron.y is time measured from telemetry data (this line assigns "chron" to miniscope data time, when it is present, and telemetry data time when it isn't)
+          df%>%filter(!is.na(chron.x))%>%rename(chron=chron.x)%>%select(!chron.y))
+
+##### Graph
+setwd(output_dir)
+
+#Lines
+p<-ggplot(df%>%filter(!is.na(YrA)),aes(x=frame,y=unit_id))+
+  geom_ridgeline(aes(height=scaled_YrA),fill=NA)+
+  ms
+save_png_large("line plot", plot=p,w=20,h=20)
+
+#Motion
+p<-ggplot(motion, aes(x=frame,y=motion_distance))+
+  geom_line()+
+  ms
+p
+save_png_large("motion",plot=p,w=20,h=6)
