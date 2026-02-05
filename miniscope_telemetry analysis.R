04c7ebabc89ef5d43c181298b8329a2a06dca6fd
Paul Vander <109230752+pbvander@users.noreply.github.com>
2026-02-04 17:07:44 -0800

    Add group_gonad to metadata

--- miniscope_telemetry analysis.R
+++ miniscope_telemetry analysis.R
@@ -84,7 +84,7 @@ df<-merge(C,S)%>%
   merge(motion)%>%
   mutate(start_ts = ymd_hms(paste(start_date, start_time)),
          miniscope_ts = (start_ts + milliseconds(time_ms)),
-         
+         group_gonad = factor(paste0(group,"_",gonad),levels=c("veh_intact","veh_ovx","e2_intact","e2_ovx"))
          )%>%
   mutate(unit_id_id = paste0(mouse,"_",start_date,"_",session,"_",unit_id),
          session_id = paste0(mouse,"_",start_date,"_",session))%>%
